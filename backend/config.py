09eb41c1a357c2889336df305ee76cc9eb13e59b
Aditya-Kayasth <adikayasth@gmail.com>
2025-03-19 14:51:46 +0530

    Added confugurations and  updated requirements.txt

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,6 @@
 import os
 
-NEWS_API_KEY = "pub_713126568809cbb479e396fcef2b6c9d38644"
-NEWS_API_URL = "https://newsdata.io/api/1/news"
+NEWSAPI_KEY = os.environ.get("NEWSAPI_KEY", "a41360b0efc146388b0db0a7051b7e4f")
 
 SECRET_KEY = os.getenv("JWT_SECRET", "supersecretkey")
 TOKEN_EXPIRY_HOURS = 24
